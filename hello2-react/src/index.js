766276fbf269bb576e8b84534f875a3ac6e07652
jiekekeji <jieke123>
2017-03-17 17:53:21 +0800

    react路由入门一

--- hello2-react/src/index.js
+++ hello2-react/src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router'
+import {Router, Route} from 'react-router'
 import App from './App';
 import './index.css';
 
 import About from './components/About'
 import Repos from './components/Repos'
 
+
 ReactDOM.render((
-    <Router history={browserHistory}>
-        <Route path="/" component={App}/>
-        {/* add the routes here */}
-        <Route path="/repos" component={Repos}/>
-        <Route path="/about" component={About}/>
+    <Router>
+        <Route path="/" component={App}>
+            <Route path="about" component={About}/>
+            <Route path="repos" component={Repos}/>
+        </Route>
     </Router>
 ), document.getElementById('root'))
